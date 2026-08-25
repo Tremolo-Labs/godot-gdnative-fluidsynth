03569ef4627f2e273a28f26b082d8665a1bc2d62
Chad Cassady <chad.cassady@gmail.com>
2026-08-25 09:48:45 -0500

    remove spurious file-loader workaround (#29)
    my local build was broken with a stale binary or something, the LLM hallucinated and failed to see the try/catch block right there, I failed to get adequate rest and do the same

--- src/godot_fluidsynth.h
+++ src/godot_fluidsynth.h
@@ -23,7 +23,7 @@ class GDMidiAudioStreamPlayer : public AudioStreamPlayer {
 private:
 	String soundfont;
 	String midi_file;
-	String temp_path;
+	Ref<SoundFontFileReader> soundfont_resource;
 	float *buffer;
 	bool fluidsynth_playing;
 	int sfont_id;
